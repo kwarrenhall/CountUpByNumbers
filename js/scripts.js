0b8d3f357e913ba7767a5edfb7a17a1b3dcc4279
kate hall and Pablo Vera <kwarrenhall@gmail.com>
2016-01-21 15:21:42 -0800

    add working front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+// front end*********
+$(document).ready(function(){
+  $("form").submit(function(event) {
+  var userNumber = parseInt($("input#number").val());
+  var userCounter = parseInt($("input#counter").val());
+  var result = countUp(userNumber, userCounter);
+  $("#outPut").text(result);
+  $(".result").show();
+  event.preventDefault();
+  });
+});
 
 
-
+// back end*********
 function countUp(number, counter) {
   var outPut = [];
   for (var i = counter; i <= number; i += counter){
