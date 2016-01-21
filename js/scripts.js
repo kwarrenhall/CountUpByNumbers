89e61d2391f600ed670abfa700c88de1a9a100c7
kate hall and Pablo Vera <kwarrenhall@gmail.com>
2016-01-21 11:51:34 -0800

    add styling and first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 
-$(document).ready(function() {
-  $("form#countByup").submit(function(event) {
-    var **** = ***($("input#****").val());
+// $(document).ready(function() {
+//   $("form#countByup").submit(function(event) {
+//     var **** = ***($("input#****").val());
+//
+//     // $(".translator").text(translator);
+//
+//     $("#result").show();
+//     event.preventDefault();
+//   });
+// });
 
-    // $(".translator").text(translator);
+var countBy = function add(number) {
+  return +1}
 
-    $("#result").show();
-    event.preventDefault();
-  });
-});
+
+
+
+//   for (i= 0; 1 < 30; i++) {
+//   if (i<30 === true) number++;
+//   }
+// return number}
+
+
+
+
+  $(document).ready(function() {
+
+  }
--- spec/specs.js
+++ spec/specs.js
@@ -1,22 +1,34 @@
-describe('vowelCheck', function() {
-  it("is true for any word that starts with a vowel", function() {
-      expect(vowelCheck("another")).to.equal(true);
+describe("countBy", function() {
+  it("counts up by 1 and it stops at 30", function() {
+    expect(countBy(1)).to.equal(30);
   });
-  it("is false for any word that starts with a consonant", function() {
-        expect(vowelCheck("banana")).to.equal(false);
-    });
-    it("is true for any word that starts with qu", function() {
-        expect(quCheck("question")).to.equal(true);
-    });
-    it("is true for any word that starts with an y", function() {
-        expect(yCheck("yoga")).to.equal(true);
-    });
-    it("exists if a word returns", function() {
-      expect(translate("another")).to.equal("anotheray");
-    });
-    it("parses to an array if input is a sentance", function() {
-      var parsedTest = ["a", "long", "sentence"];
-      expect(sentenceParse("a long sentence")).to.eql(parsedTest);
-    });
+})
 
-});
+
+
+
+
+
+
+// describe('vowelCheck', function() {
+//   it("is true for any word that starts with a vowel", function() {
+//       expect(vowelCheck("another")).to.equal(true);
+//   });
+//   it("is false for any word that starts with a consonant", function() {
+//         expect(vowelCheck("banana")).to.equal(false);
+//     });
+//     it("is true for any word that starts with qu", function() {
+//         expect(quCheck("question")).to.equal(true);
+//     });
+//     it("is true for any word that starts with an y", function() {
+//         expect(yCheck("yoga")).to.equal(true);
+//     });
+//     it("exists if a word returns", function() {
+//       expect(translate("another")).to.equal("anotheray");
+//     });
+//     it("parses to an array if input is a sentance", function() {
+//       var parsedTest = ["a", "long", "sentence"];
+//       expect(sentenceParse("a long sentence")).to.eql(parsedTest);
+//     });
+//
+// });
