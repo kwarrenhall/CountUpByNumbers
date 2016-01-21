9f975cae4a2752b3860eb767c43ebf552a87e8df
kate hall and Pablo Vera <pablovera.pdx@gmail.com>
2016-01-21 14:29:21 -0800

    add first test for counting by multiple of 5 up to 30

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,10 @@
 
-// $(document).ready(function() {
-//   $("form#countByup").submit(function(event) {
-//     var **** = ***($("input#****").val());
-//
-//     // $(".translator").text(translator);
-//
-//     $("#result").show();
-//     event.preventDefault();
-//   });
-// });
-
-var countBy = function add(number) {
-  return +1}
 
 
-
-
-//   for (i= 0; 1 < 30; i++) {
-//   if (i<30 === true) number++;
-//   }
-// return number}
-
-
-
-
-  $(document).ready(function() {
-
+function countUp(number, counter) {
+  var outPut = [];
+  for (var i = counter; i <= number; i += counter){
+  outPut.push(i);
   }
+  return outPut;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,34 +1,5 @@
-describe("countBy", function() {
-  it("counts up by 1 and it stops at 30", function() {
-    expect(countBy(1)).to.equal(30);
+describe("countUp",function(){
+  it("will count up to 30 by increments of 5", function() {
+    expect(countUp(30, 5)).to.eql([5,10,15,20,25,30]);
   });
-})
-
-
-
-
-
-
-
-// describe('vowelCheck', function() {
-//   it("is true for any word that starts with a vowel", function() {
-//       expect(vowelCheck("another")).to.equal(true);
-//   });
-//   it("is false for any word that starts with a consonant", function() {
-//         expect(vowelCheck("banana")).to.equal(false);
-//     });
-//     it("is true for any word that starts with qu", function() {
-//         expect(quCheck("question")).to.equal(true);
-//     });
-//     it("is true for any word that starts with an y", function() {
-//         expect(yCheck("yoga")).to.equal(true);
-//     });
-//     it("exists if a word returns", function() {
-//       expect(translate("another")).to.equal("anotheray");
-//     });
-//     it("parses to an array if input is a sentance", function() {
-//       var parsedTest = ["a", "long", "sentence"];
-//       expect(sentenceParse("a long sentence")).to.eql(parsedTest);
-//     });
-//
-// });
+  });
